9e5753e63cf16ff1725d0eb334cc5c3a71b77e63
Soehtun <soehtuncu@gmail.com>
2020-09-01 15:05:14 +0630

    add download screen image

--- src/components/QR_Genarator.vue
+++ src/components/QR_Genarator.vue
@@ -5,10 +5,12 @@
             <textarea v-model="textarea" rows="3"></textarea><br>
             <button @click="generateQR" >Generate QR Code</button>
         </div>
+        <button @click="get()">Get Screenshot</button>
     </div>
 </template>
 
 <script>
+import html2canvas from 'html2canvas'
 const baseUrl = "http://api.qrserver.com/v1/create-qr-code/";
 export default {
     name:'test',
@@ -19,15 +21,37 @@ export default {
     },
     methods:{
         generateQR(){
-                let size = "1000x1000";
-                let data = this.textarea;
-                let qrcode = document.querySelector("img");
-                console.log(qrcode);
-                console.log("HI==>",data);
-                
-                let url = `${baseUrl}?data=${data}&size=${size}`;
-                qrcode.src =url
+            //let size = "1000x1000";
+            let data = this.textarea;
+            let qrcode = document.querySelector("img");
+            console.log(qrcode);
+            console.log("HI==>",data);
+            
+            let url = `${baseUrl}?data=${data}`;
+            qrcode.src =url
+        },
+        get(){
+            //take screenshot and download this image
+            html2canvas(document.querySelector(".qrbox")).then(canvas => {
+                document.body.appendChild(canvas)
+                }).then(() => {
+                    var canvas = document.querySelector('canvas');
+                    canvas.style.display = 'none';
+                    var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+                    var a = document.createElement("a");
+                    a.setAttribute('download', 'myImage.png');
+                    a.setAttribute('href', image);
+                    a.click();
+            })
+        },
+        onCLick(e){
+            if(e.keyCode === 13){
+                this.generateQR()
             }
+        }
+    },
+    created(){
+        window.addEventListener("keydown", this.onCLick)
     }
 }
 </script>
@@ -50,8 +74,9 @@ button{
     width: 220px;
 }
 img{
-    width: 220px;
-    height: 200px;
+    width: 200px;
+    height: 180px;
+    padding: 10px;
     border: 1px solid rgb(9, 145, 9);
     outline: none;
 }
